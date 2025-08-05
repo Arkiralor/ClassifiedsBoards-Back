1dc2dc37a8f86274502413d882a801663d5f49c2
Prithoo Medhi <prithoo11335@gmail.com>
2025-08-05 12:50:32 +0530

    new methods

--- classifieds_app/helpers.py
+++ classifieds_app/helpers.py
@@ -172,7 +172,7 @@ class ClassifiedsCategoryHelper:
 
         logger.info(resp.to_text())
         return resp
-    
+
     @classmethod
     def update(cls, user: User = None, category_id: str = None, data: dict = None, return_obj: bool = False, *args, **kwargs) -> Resp:
         resp = Resp()
@@ -202,17 +202,12 @@ class ClassifiedsCategoryHelper:
                 logger.warning(resp.to_text())
                 return resp
 
-        obj = ClassifiedsCategory.objects.filter(pk=category_id).first()
-        if not obj:
-            resp.error = f"INVALID CATEGORY ID"
-            resp.message = f"Category with ID {category_id} does not exist."
-            resp.status_code = status.HTTP_404_NOT_FOUND
-
-            logger.warning(resp.to_text())
-            return resp
+        obj = cls.get(category_id=category_id, return_obj=True)
+        if not obj.error:
+            return obj
 
         deserialized = ClassifiedsCategoryIOSerializer(
-            instance=obj, data=data, partial=True)
+            instance=obj.data, data=data, partial=True)
         if not deserialized.is_valid():
             resp.error = f"INVALID DATA"
             resp.message = f"{deserialized.errors}"
@@ -228,7 +223,7 @@ class ClassifiedsCategoryHelper:
 
         logger.info(resp.to_text())
         return resp
-    
+
     @classmethod
     def delete(cls, user: User = None, category_id: str = None, *args, **kwargs) -> Resp:
         resp = Resp()
@@ -249,18 +244,151 @@ class ClassifiedsCategoryHelper:
             logger.warning(resp.to_text())
             return resp
 
-        obj = ClassifiedsCategory.objects.filter(pk=category_id).first()
+        obj = cls.get(category_id=category_id, return_obj=True)
+        if obj.error:
+            return obj
+
+        obj.data.delete()
+        resp.message = f"Category '{category_id}' deleted successfully."
+        resp.status_code = status.HTTP_200_OK
+
+        logger.info(resp.to_text())
+        return resp
+
+
+class ClassifiedsAdvertisementHelper:
+
+    EDITABLE_FIELDS = (
+        'title',
+        'description',
+        'price',
+        'category',
+    )
+
+    @classmethod
+    def get_one(cls, user: User = None, pk: str = None, return_obj: bool = False, *args, **kwargs) -> Resp:
+        resp = Resp()
+        obj: ClassifiedsAdvertisement = None
+
+        if not pk or not isinstance(pk, str) or pk == StringConstants.BLANK:
+            resp.error = f"INVALID INPUT"
+            resp.message = f"Advertisement ID must be provided."
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+
+            logger.warning(resp.to_text())
+            return resp
+
+        obj = ClassifiedsAdvertisement.objects.filter(pk=pk).first()
+        if (not obj.creator == user) and (not user in obj.moderators.all()) and (not user.is_superuser) and (not obj.is_active):
+            obj = None
+
         if not obj:
-            resp.error = f"INVALID CATEGORY ID"
-            resp.message = f"Category with ID {category_id} does not exist."
+            resp.error = f"NOT FOUND"
+            resp.message = f"Advertisement with ID {pk} does not exist or is inactive"
             resp.status_code = status.HTTP_404_NOT_FOUND
 
             logger.warning(resp.to_text())
             return resp
 
-        obj.delete()
-        resp.message = f"Category '{obj.name}' deleted successfully."
+        resp.message = f"Advertisement '{obj.title}' found successfully."
+        resp.data = obj if return_obj else ClassifiedsAdvertisementDisplaySerializer(
+            obj).data
         resp.status_code = status.HTTP_200_OK
 
         logger.info(resp.to_text())
+        return resp
+
+    @classmethod
+    def search(cls, query: str = None, return_obj: bool = False, page_no: int = 1, *args, **kwargs) -> Resp:
+        resp = Resp()
+
+        if not isinstance(query, str):
+            resp.error = f"INVALID INPUT"
+            resp.message = f"Search query must be a string."
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+
+            logger.warning(resp.to_text())
+            return resp
+
+        query = query.lstrip().rstrip().lower() if query else None
+        if not query or query == StringConstants.BLANK:
+            resp.message = f"No search query provided. Returning all advertisements."
+            objs = ClassifiedsAdvertisement.objects.filter(
+                is_active=True).order_by('-created')
+            resp.data = objs if return_obj else ClassifiedsAdvertisementOutputSerializer(
+                objs, many=True).data
+            resp.status_code = status.HTTP_200_OK
+
+            logger.info(resp.to_text())
+            return resp
+
+        objs = ClassifiedsAdvertisement.objects.filter(
+            Q(is_active=True)
+            & Q(
+                Q(title__trigram_similar=query)
+                | Q(description__trigram_similar=query)
+                | Q(category__name__trigram_similar=query)
+            )
+        ).distinct().annotate(
+            similarity=(TrigramSimilarity('title', query) * 1.5)
+            + (TrigramSimilarity('description', query) * 1.2)
+            + (TrigramSimilarity('category__name', query) * 1.0)
+        ).order_by('similarity')
+
+        paginated = Paginator(objs, django_settings.MAX_ITEMS_PER_PAGE)
+        page = paginated.get_page(page_no)
+
+        resp.message = f"Advertisements found successfully matching the query '{query}'."
+        resp.data = page if return_obj else ClassifiedsAdvertisementDisplaySerializer(
+            page, many=True).data
+        resp.status_code = status.HTTP_200_OK
+
+        logger.info(resp.to_text())
+        return resp
+
+    @classmethod
+    def create(cls, user: User = None, data: dict = None, return_obj: bool = False, *args, **kwargs) -> Resp:
+        resp = Resp()
+
+        if not user or not isinstance(user, User):
+            resp.error = f"INVALID INPUT"
+            resp.message = f"User must be provided."
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+
+            logger.warning(resp.to_text())
+            return resp
+
+        if not data or not isinstance(data, dict):
+            resp.error = f"INVALID INPUT"
+            resp.message = f"Data must be a dictionary."
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+
+            logger.warning(resp.to_text())
+            return resp
+
+        category = data.get('category')
+        if isinstance(category, dict):
+            create_category = ClassifiedsCategoryHelper.create(
+                data=category, return_obj=True)
+            if create_category.error:
+                return create_category
+            data['category'] = create_category.data.id
+
+        data['creator'] = user.id
+        data['is_active'] = False  # Default to inactive until approved by SITE moderators
+        deserialized = ClassifiedsAdvertisementInputSerializer(data=data)
+        if not deserialized.is_valid():
+            resp.error = f"INVALID DATA"
+            resp.message = f"{deserialized.errors}"
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+
+            logger.warning(resp.to_text())
+            return resp
+
+        deserialized.save()
+        resp.message = f"Advertisement '{deserialized.data.get('title')}' created successfully."
+        resp.data = deserialized.instance if return_obj else ClassifiedsAdvertisementDisplaySerializer(deserialized.instance).data
+        resp.status_code = status.HTTP_201_CREATED
+
+        logger.warning(resp.to_text())
         return resp
